0f78135464b9dc81d255a5a00f057863b2f14686
vanboi1 <vanjazzmvf1996@gmail.com>
2025-11-15 02:25:11 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import { translations } from './translations';
+import { translations } from './Translations';
 
 i18n
   .use(LanguageDetector)
